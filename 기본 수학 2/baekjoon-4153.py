5d4c65f38cf51e4c990b09cc4c6a1080d13b45e4
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-02-17 15:26:17 +0900

    solved : [4153]

---
+++ 기본 수학 2/baekjoon-4153.py
@@ -0,0 +1,10 @@
+while 1:
+    a,b,c = map(int,input().split())
+    if a+b+c == 0:
+        break
+    array = [a,b,c]
+    array.sort()
+    if (array[0]**2) + (array[1]**2) == (array[2]**2):
+        print("right")
+    else:
+        print("wrong")
