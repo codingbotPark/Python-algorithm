18b3c2edb7ab364f229135be833a99f48c516ee7
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-02-22 20:32:04 +0900

    solved : [10870]

--- 재귀/baekjoon-10870.py
+++ 재귀/baekjoon-10870.py
@@ -1,12 +1,27 @@
-num = 0
-def fibo(n):
-    if n == 2:
+
+def fibo(num):
+    if num == 0:
+        return 0
+    elif num == 1 or num == 2:
         return 1
-    return 1 + fibo(n-1)
-n = int(input())
-if n == 0:
-    print(0)
-elif n==1:
-    print(1)
-else:
-    print(fibo(n))
+    else:
+        return fibo(num - 1) + fibo(num - 2)
+
+num = int(input())
+print(fibo(num))
+
+
+# 시행착오▼
+# 재귀가 없을 때의 피보나치 수열 코드 
+# num = int(input())
+# if num == 0:
+#     print(0)
+# else:
+#     temp1 = 0
+#     temp2 = 1
+#     temp3 = 0
+#     for i in range(num-1):
+#         temp3 = temp1 + temp2
+#         temp1 = temp2
+#         temp2 = temp3
+#     print(temp3)
