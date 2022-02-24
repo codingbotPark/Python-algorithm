b1fd7f946ac8982e09450a1fb55acece9955318a
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-02-24 13:40:20 +0900

    solving : [2447]

--- 재귀/baekjoon-2447.py
+++ 재귀/baekjoon-2447.py
@@ -1,6 +1,34 @@
 
 
+
 # 시행착오▼
+# 결국 모양들은 아래와 같은 모양이 단위체가 된다 할 수 있다
+# ***
+# * *
+# ***
+# 이런 단위체를 만들 때 마다 저장해서 늘려간다
+
+
+# num = "**** ****"
+# n = int(input())
+# for i in range(1,len(num)+1):
+#     print(num[i-1],end='')
+#     if i % n == 0:
+#         print()
+
+# stars = "**** ****"
+# array = []
+# for i in range(9):
+#     array.append(stars)
+#     print(array)
+
+
+
+# 시행착오▼
+# 입력받은 수 만큼 2차원 배열은 만들어 각 인덱스들을
+# 판별함수에 넣어 비었다면 = 0, 별이 있다면 = 1
+# 하지만 판별한는 과정이 어려웠고 비효율적인 것 같았다
+# 
 #  # 비었다면 0, 별이 있다면 1을 return
 # def isEmpty(i,j):
 #     for c in range(m):
@@ -39,3 +67,8 @@
 #         else:
 #             print(" ",end="")
 #     print()
+
+
+
+
+
