8871ab85bb381accb0cff63e792e14b0cfa4e3a8
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-02-28 23:32:54 +0900

    solved : [2447] (pypy3)

--- 재귀/baekjoon-2447(pypy3).py
+++ 재귀/baekjoon-2447(pypy3).py
@@ -4,22 +4,20 @@
 # 6으로 나눌 때 = 3, 4, 5
 # 9로 나눌 때 = 1
 
+
+# pypy3로 했을 때 맞는 코드
 def isStar(i,j):
-    # for c in range(1,numTimes+1):
-    #     cTime = c ** 3
-    #     for C in range(3 ** (c-1)):
-    #         if i % cTime > (cTime ):
-    #             return 0
-    # return 1
-    for c in range(1,numTimes + 1):
-        cTime = c ** 3 # 3의 제곱
-        # if i % cTime > 
-        if (((i / cTime)%1 > 1/3) and ((i / cTime)%1 < 2/3)) and\
-        (((j / cTime)%1 > 1/3) and ((j / cTime)%1 < 1)):
-            return 0
+    for c in range(1,numTimes+1):
+        cTime = 3 ** c  # 3의 n제곱
+
+        if (i % cTime >= (cTime / 3)) and (i % cTime < ((cTime / 3) * 2)):
+            if (j % cTime >= (cTime / 3)) and (j % cTime < ((cTime / 3) * 2)):
+                return 0
+
+
     return 1
 
-num = int(input())
+num = int(input()) 
 temp = num
 numTimes = 0
 while temp > 1:
@@ -28,13 +26,50 @@ while temp > 1:
 if numTimes: # num이 1이 아닐 때
     for i in range(num):
         for j in range(num):
-            if isStar(i+1,j+1):
+            if isStar(i,j):
                 print("*",end="")
             else:
                 print(" ",end="")
         print()
 else: # num이 1일 때
     print("*")
+
+
+
+
+# 시행착오▼
+# 같은 개념
+# def isStar(i,j):
+#     # for c in range(1,numTimes+1):
+#     #     cTime = c ** 3
+#     #     for C in range(3 ** (c-1)):
+#     #         if i % cTime > (cTime ):
+#     #             return 0
+#     # return 1
+#     for c in range(1,numTimes + 1):
+#         cTime = c ** 3 # 3의 제곱
+#         # if i % cTime > 
+#         if (((i / cTime)%1 > 1/3) and ((i / cTime)%1 < 2/3)) and\
+#         (((j / cTime)%1 > 1/3) and ((j / cTime)%1 < 1)):
+#             return 0
+#     return 1
+
+# num = int(input())
+# temp = num
+# numTimes = 0
+# while temp > 1:
+#     temp /= 3
+#     numTimes+=1
+# if numTimes: # num이 1이 아닐 때
+#     for i in range(num):
+#         for j in range(num):
+#             if isStar(i+1,j+1):
+#                 print("*",end="")
+#             else:
+#                 print(" ",end="")
+#         print()
+# else: # num이 1일 때
+#     print("*")
 
 
 
@@ -110,26 +145,26 @@ else: # num이 1일 때
 
 # https://github.com/jongpark1234 코드
 
-# from sys import setrecursionlimit
-# setrecursionlimit(10 ** 6)
-# n = int(input())
-# star = [[' ' for i in range(n)] for j in range(n)]
-# def staring(num):
-#     if num == 3:
-#         star[0][:3] = star[2][:3] = ['*'] * 3
-#         star[1][:3] = ['*', ' ', '*']
-#         return
-#     num //= 3
-#     staring(num)
-#     for i in range(3):
-#         for j in range(3):
-#             if i == j == 1:
-#                 continue
-#             for k in range(num):
-#                 star[num * i + k][num * j:num * (j + 1)] = star[k][:num]
-# staring(n)
-# for i in star:
-#     print(''.join(i))
+from sys import setrecursionlimit
+setrecursionlimit(10 ** 6)
+n = int(input())
+star = [[' ' for i in range(n)] for j in range(n)]
+def staring(num):
+    if num == 3:
+        star[0][:3] = star[2][:3] = ['*'] * 3
+        star[1][:3] = ['*', ' ', '*']
+        return
+    num //= 3
+    staring(num)
+    for i in range(3):
+        for j in range(3):
+            if i == j == 1:
+                continue
+            for k in range(num):
+                star[num * i + k][num * j:num * (j + 1)] = star[k][:num]
+staring(n)
+for i in star:
+    print(''.join(i))
 
 
 
