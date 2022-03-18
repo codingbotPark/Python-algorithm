c1388ea8ebb98529c667322542e6c6fd765bbcbc
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-03-18 11:17:05 +0900

    solving : [2108]

--- 정렬/baekjoon-2108.py
+++ 정렬/baekjoon-2108.py
@@ -1,6 +1,37 @@
 
-# 시행착오▼
-# 최빈값 조건 중 '여러 개 있을 때는 최빈값 중 두 번째로 작은 값을 출력한다'를 구현하지 못함
+# # 시행착오▼
+# # 왜인진 모르겠지만 틀린다고 뜬다
+# import sys
+# num = int(sys.stdin.readline())
+# array1 = [0]*8001 # + 하며 추가 
+# array2 = []
+# sum = 0
+# for i in range(num):
+#     array2.append(int(sys.stdin.readline()))
+#     array1[array2[i]+4000]+=1
+#     sum += array2[i]
+# print(round(sum/num))
+# array2.sort()
+# if ((num/2) % 1) > 0.4:
+#     num = ((num / 2) % 1)+1
+# else:
+#     num = ((num/2)%1)-1
+# print(array2[round(num)])
+# freNumber = max(array1) #가장 큰 수
+# counter = 0
+# for i in range(len(array1)):
+#     if array1[i] == freNumber:
+#         counter += 1
+#         value = i
+#         if counter == 2:
+#             value = i
+#             break
+# print(value - 4000)
+# print(array2[-1] - array2[0])
+
+
+# # 시행착오▼
+# # 최빈값 조건 중 '여러 개 있을 때는 최빈값 중 두 번째로 작은 값을 출력한다'를 구현하지 못함
 # import sys
 # num = int(sys.stdin.readline())
 # array = []
