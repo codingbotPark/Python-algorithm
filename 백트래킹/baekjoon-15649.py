fb9911a29c45673c5fd182d5598e64456bc0718d
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-03-30 18:23:06 +0900

    skip : [18870] solving : [15649]

---
+++ 백트래킹/baekjoon-15649.py
@@ -0,0 +1,6 @@
+
+# 1~n까지의 수 중 m개를
+n,m = map(int,input().split())
+for i in range(n):
+    for j in range(m):
+        
--- 정렬/baekjoon-18870(SKIP).py
+++ 정렬/baekjoon-18870(SKIP).py
@@ -1,3 +1,28 @@
+# 더 효과적으로 딕셔너리를 사용해야한다
+import sys
+case = int(sys.stdin.readline())
+array = []
+array = list(sys.stdin.readline().split())
+dictionary = {i:array[i] for i in range(len(array))}
+print(dictionary)
+
+
+# 딕셔너리를 써서 아래처럼 각각 매칭되게 해야한다
+# 1 1 3 3 2 2 6 6 5 5
+# 0 0 2
+
+# 0 0 2 2 1 1 4 4 3 3
+
+# 1 2 3 5 6
+# {
+#     1:0
+#     2:1
+#     3:2
+#     5:3
+#     6:4
+# }
+
+
 # 시행착오▼
 # 시간초과
 # import sys
