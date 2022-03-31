592f04f9502409b1cca4790517879a106521b0ea
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-03-31 12:35:10 +0900

    solving : [15649]

--- 백트래킹/baekjoon-15649.py
+++ 백트래킹/baekjoon-15649.py
@@ -1,6 +1,16 @@
 
-# 1~n까지의 수 중 m개를
-n,m = map(int,input().split())
-for i in range(n):
-    for j in range(m):
-        
+
+# import sys
+# a,b = map(int,sys.stdin.readline().split())
+# for x in range(1,a+1):
+#     for y in range(1,a+1):
+#         for z in range()
+#     print()
+
+# import sys
+# a,b = map(int,sys.stdin.readline().split())
+# for i in range(1,a+1):
+#     for j in range(1,a+1):
+#         for m in range(b):
+#             print(i,j)
+#         print()
