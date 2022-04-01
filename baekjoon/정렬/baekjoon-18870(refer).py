91f51deaaa5e643bf6bb359c30a50a65538fe0a1
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-04-01 20:45:34 +0900

    solved : [18870] | update README

--- baekjoon/정렬/baekjoon-18870(refer).py
+++ baekjoon/정렬/baekjoon-18870(refer).py
@@ -1,10 +1,33 @@
 # 더 효과적으로 딕셔너리를 사용해야한다
-import sys
-case = int(sys.stdin.readline())
+
+# 박종현과의 통화
+# 사전이란 객체와 같은 느낌,
+# 키 값과, 값이 있듯
+# 
+# 딕셔너리를 만드는 방법에는 
+# A = [1,2,3]
+# B = [5,6,7]
+# A = B
+# A[0] = 1
+# 를 하면 1,6,7이 나오는데 이는 얕은복사라고 하고
+# 이를 없애려면 A = B[:] (슬라이싱)를 해야한다
+# 
+
+
+case = int(input())
 array = []
-array = list(sys.stdin.readline().split())
-dictionary = {i:array[i] for i in range(len(array))}
-print(dictionary)
+Dict = {}
+# array = map(int,list(input())) # 2 4 -10 4 -9
+array = list(map(int,input().split()))
+tempArray = list(set(array))
+tempArray.sort()
+
+for i in range(len(tempArray)):
+    # 딕셔너리에 key값으로 tempArray의 값이 들어가고
+    # 값으로는 i가 들어온다
+    Dict[tempArray[i]] = i
+for i in array:
+    print(Dict[i], end=" ")
 
 
 # 딕셔너리를 써서 아래처럼 각각 매칭되게 해야한다
