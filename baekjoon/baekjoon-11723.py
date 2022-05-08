a7fc57cbbfa5f78712c722647e88aba7ee3dda52
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-05-08 21:41:48 +0900

    solved : [11723] solving : [4378]

--- baekjoon/baekjoon-11723.py
+++ baekjoon/baekjoon-11723.py
@@ -5,21 +5,20 @@
 
 import sys
 
-arr = []
+arr = [False] * 20
 
 def check(x):
-    if x in arr:
-        return 1
-    else:
-        return 0
+    # x가 있을 때 true
+    return arr[x-1]
+
+def remove(x):
+    # 있을 때 삭제
+    if check(x):
+        arr[x-1] = False
 
 def add(x):
     if not(check(x)):
-        arr.append(x)
-
-def remove(x):
-    if check(x):
-        arr.remove(x)
+        arr[x-1] = True
 
 def toggle(x):
     if check(x):
@@ -27,28 +26,79 @@ def toggle(x):
     else:
         add(x)
 
-def empty():
-    arr.clear()
-
 def all():
-    empty()
-    (lambda x:add(x))(range(1,21))
+    for i in range(20):
+        arr[i] = True
+
+def empty():
+    for i in range(20):
+        arr[i] = False
 
 for i in range(int(input())):
-    value = list(sys.stdin.readline().split())
-    cal = value[0]
-    if cal == "add":
-        add(value[1])
-    elif cal == "remove":
-        remove(value[1])
-    elif cal == "toggle":
-        toggle(value[1])
-    elif cal == "check":
-        print(check(value[1]))
-    elif cal == "empty":
-        empty()
-    else:
+    inputV = list(sys.stdin.readline().split())
+    # print(inputV)
+    if inputV[0] == "add":
+        add(int(inputV[1]))
+    elif inputV[0] == "remove":
+        remove(int(inputV[1]))
+    elif inputV[0] == "check":
+        if check(int(inputV[1])):
+            print(1)
+        else:
+            print(0)
+    elif inputV[0] == "toggle":
+        toggle(int(inputV[1]))
+    elif inputV[0] == "all":
         all()
+    elif inputV[0] == "empty":
+        empty()
+
+# import sys
+
+# arr = []
+
+# def check(x):
+#     if x in arr:
+#         return 1
+#     else:
+#         return 0
+
+# def add(x):
+#     if not(check(x)):
+#         arr.append(x)
+
+# def remove(x):
+#     if check(x):
+#         arr.remove(x)
+
+# def toggle(x):
+#     if check(x):
+#         remove(x)
+#     else:
+#         add(x)
+
+# def empty():
+#     arr.clear()
+
+# def all():
+#     empty()
+#     (lambda x:add(str(x)))(range(1,21))
+
+# for i in range(int(input())):
+#     value = list(sys.stdin.readline().split())
+#     cal = value[0]
+#     if cal == "add":
+#         add(value[1])
+#     elif cal == "remove":
+#         remove(value[1])
+#     elif cal == "toggle":
+#         toggle(value[1])
+#     elif cal == "check":
+#         print(check(value[1]))
+#     elif cal == "empty":
+#         empty()
+#     elif cal == "all":
+#         all()
 
 
 # 입력받은 문자를 바로 함수로 사용하기 위해 딕셔너리를 사용
--- baekjoon/baekjoon-4378(solving).py
+++ baekjoon/baekjoon-4378(solving).py
@@ -1,12 +1,31 @@
+
+
+
+
 # first = ["\`","1","2","3","4","5","6","7","8","9","0","-","="]
 # second = ["Q","W","E","R","T","Y","U","I","O","P","[","]","\\"]
 # third = ["A","S","D","F","G","H","J","K","L",";","'"]
 # fourth = ["Z","X","C","V","B","N","M",",",".","/"]
 
-keys = ["\`","1","2","3","4","5","6","7","8","9","0","-","=","Q","W","E","R","T","Y","U","I","O","P","[","]","\\","A","S","D","F","G","H","J","K","L",";","'","Z","X","C","V","B","N","M",",",".","/"]
-string = input()
-for i in string:
-    if i in keys:
-        print(keys[keys.index(i)-1],end="")
-    else:
-        print(i,end="")
+# inputV = input()
+# for i in inputV:
+#     if i in first:
+#         outV = first[first.index(i)-1]
+#     elif i in second:
+#         outV = second[second.index(i)-1]
+#     elif i in third:
+#         outV = third[third.index(i) - 1]
+#     elif i in fourth:
+#         outV = fourth[fourth.index(i)-1]
+#     else:
+#         outV = i
+#     print(outV,end="")
+
+
+# keys = ["\`","1","2","3","4","5","6","7","8","9","0","-","=","Q","W","E","R","T","Y","U","I","O","P","[","]","\\","A","S","D","F","G","H","J","K","L",";","'","Z","X","C","V","B","N","M",",",".","/"]
+# string = input()
+# for i in string:
+#     if i in keys:
+#         print(keys[keys.index(i)-1],end="")
+#     else:
+#         print(i,end="")
