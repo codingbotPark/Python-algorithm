7642dd027051a954716071dc927596cee9d0e434
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-06-04 15:40:44 +0900

    solved : [10845]

--- baekjoon/baekjoon-10845.py
+++ baekjoon/baekjoon-10845.py
@@ -1,2 +1,31 @@
-print("int",5)
-print("long",50)
+# 파이썬 배열에서 가장 앞 요소를 추가하는 방법은
+# insert이다, insert(넣을 인덱스, 넣을 값)
+
+arr = []
+
+import sys
+
+for i in range(int(input())):
+    command = (sys.stdin.readline()).split()
+    if command[0] == "push":
+        arr.insert(0,int(command[1]))
+    elif command[0] == "pop":
+        if (len(arr)):
+            print(arr.pop())
+        else: # 배열이 비었을 때
+            print(-1)
+    elif command[0] == "size":
+        print(len(arr))
+    elif command[0] == "empty":
+        # arr가 비었을 때 len(arr) == 0 은 true, int 형으로 바꿔준다
+        print(int(len(arr) == 0))
+    elif command[0] == "front":
+        if (len(arr)):
+            print(arr[len(arr) - 1])
+        else:
+            print(-1)
+    elif command[0] =="back":
+        if (len(arr)):
+            print(arr[0])
+        else:
+            print(-1)
