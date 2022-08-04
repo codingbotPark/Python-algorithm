caadb78d29bb929d2e602cc16ff5f606d3ceea4a
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-08-04 15:05:12 +0900

    refer : [24267]

---
+++ baekjoon/baekjoon-24267.py
@@ -0,0 +1,19 @@
+# https://sorryhyeon.tistory.com/m/65
+
+# n = int(input())
+# a = ((n-1)*(n-2) * ((2*n)-3)+3*(n-1)*(n-2)) // 12
+# print(a)
+# print(3)
+
+
+# # 그대로 하면 시간초과가 뜬다
+# n = int(input())
+# counter = 0
+# for i in range(1,n-1):
+#     for j in range(i+1,n):
+#         for k in range(j+1,n+1):
+#             counter += 1
+
+# print(counter)
+# print(3)
+
